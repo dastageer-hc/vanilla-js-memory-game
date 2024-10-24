65674008098e110b047df5ada13be6be18a09cb9
dastageer <dastageer76@gmail.com>
2024-10-24 22:03:13 +0530

    unique repeated patter for the boxes

--- src/js/app.js
+++ src/js/app.js
@@ -2,26 +2,33 @@ const boxContainer = document.getElementById("box-container");
 
 // Create a 4 by 4 grid
 let boxesArray = Array.from({ length: 4 }, (_, rowIndex) => {
-    return Array.from({ length: 4 }, (_, colIndex) => {
-        const newDiv = document.createElement('div');
-        newDiv.innerHTML = `${rowIndex * 4 + colIndex}`; // Display the index in a grid format
-        newDiv.classList.add("box");
-        newDiv.setAttribute("card-number", rowIndex);
-        return newDiv;
-    });
+  return Array.from({ length: 4 }, (_, colIndex) => {
+    const newDiv = document.createElement("div");
+    newDiv.innerHTML = `${rowIndex + 1 * 4 + colIndex}`; // Display the index in a grid format
+    newDiv.classList.add("box");
+    newDiv.setAttribute("card-number", rowIndex * 4 + colIndex);
+    return newDiv;
+  });
 });
 
 // Function to render the boxes in a grid format with a class for each row
 const renderTable = () => {
-    return boxesArray.map((row, rowIndex) => {
-        return `<div class="row">` + 
-            row.map((div, index) =>`<div class='outer-div' data-card-number='${index}'>
-           <div class='inner-div'> Guess
-           </div>
+  return boxesArray
+    .map((row, rowIndex) => {
+      return (
+        `<div class="row">` +
+        row
+          .map(
+            (div, index) => `<div class='outer-div' data-card-number='${index}'>
+    
             ${div.outerHTML}
-            </div>`).join('') + 
-            `</div>`;
-    }).join(''); // Join rows to return the full HTML string
+            </div>`
+          )
+          .join("") +
+        `</div>`
+      );
+    })
+    .join(""); // Join rows to return the full HTML string
 };
 
 // Insert the rendered grid into the container
@@ -31,6 +38,6 @@ ${renderTable()}
 
 const table = document.querySelector(".table");
 table.addEventListener("click", (event) => {
-    console.log(event.target, 'evnet');
-})
+  console.log(event.target, "evnet");
+});
 console.log(table);
