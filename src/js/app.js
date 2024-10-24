c042505061ce18bf800ef9db0cb684b12ce655b6
dastageer <dastageer76@gmail.com>
2024-10-25 01:49:27 +0530

    [v1] completed, working version desktop, non responsive

--- src/js/app.js
+++ src/js/app.js
@@ -1,12 +1,47 @@
-const boxContainer = document.getElementById("box-container");
+// utility methods
+export function generateRandomBoxNumber(params) {
+  const randomNumber = [...params];
+  for (let i = 0; i < randomNumber.length; i++) {
+    const randomIndex = Math.floor(Math.random() * randomNumber.length);
+    [randomNumber[i], randomNumber[randomIndex]] = [
+      randomNumber[randomIndex],
+      randomNumber[i],
+    ];
+  }
+  return randomNumber;
+}
+
+const boxContainer = document.getElementById("boxes-container");
+
+// const newSet = new Set(
+//   Array.from({ length: 8 }).map(
+//     (_, index) => Math.floor(Math.random() + 1 * 16) + index
+//   )
+// );
+
+// const randomBoxNumbers = newSet.flatMap((i) => [i, i]);
+const randomSet = new Set();
+const grid_size = 4 * 4;
+
+while (randomSet.size < 8) {
+  randomSet.add(Math.floor((Math.random() + 1) * grid_size));
+}
+const duplicateBoxNumber = [...randomSet].flatMap((element) => {
+  return [element, element];
+});
+
+const randomBoxNumbers = generateRandomBoxNumber(duplicateBoxNumber);
 
 // Create a 4 by 4 grid
 let boxesArray = Array.from({ length: 4 }, (_, rowIndex) => {
   return Array.from({ length: 4 }, (_, colIndex) => {
     const newDiv = document.createElement("div");
-    newDiv.innerHTML = `${rowIndex + 1 * 4 + colIndex}`; // Display the index in a grid format
-    newDiv.classList.add("box");
-    newDiv.setAttribute("card-number", rowIndex * 4 + colIndex);
+    newDiv.innerHTML = `${randomBoxNumbers[rowIndex * 4 + colIndex]}`; // Display the index in a grid format
+    newDiv.classList.add("box", "box-back");
+    newDiv.setAttribute(
+      "data-box-number",
+      randomBoxNumbers[rowIndex * 4 + colIndex]
+    );
     return newDiv;
   });
 });
@@ -18,12 +53,17 @@ const renderTable = () => {
       return (
         `<div class="row">` +
         row
-          .map(
-            (div, index) => `<div class='outer-div' data-card-number='${index}'>
-    
-            ${div.outerHTML}
-            </div>`
-          )
+          .map((div) => {
+            return `
+                <div class='box-container' >
+                 <div class='box box-front' data-box-number='${div.getAttribute(
+                   "data-box-number"
+                 )}'>
+                  ?
+                 </div>
+                  ${div.outerHTML}
+                </div>`;
+          })
           .join("") +
         `</div>`
       );
@@ -37,7 +77,53 @@ ${renderTable()}
 </div>`;
 
 const table = document.querySelector(".table");
+
+// ----- game logic -----
+let score = 0;
+const scoreDiv = document.querySelector(".score");
+scoreDiv.innerHTML = "0";
+let currentSelectedPair = [];
+
+function checkPair() {
+  currentSelectedPair.map((target) => {
+    target.parentNode.classList.add("active");
+    target.parentNode.setAttribute("data-matched", 1);
+  });
+
+  if (currentSelectedPair.length == 2) {
+    if (
+      currentSelectedPair[0]?.getAttribute("data-box-number") ===
+      currentSelectedPair[1]?.getAttribute("data-box-number")
+    ) {
+      currentSelectedPair.map((target) => {
+        target.parentNode.classList.add("match-animation");
+      });
+      score++;
+      scoreDiv.innerHTML = `${score}`;
+    } else {
+      currentSelectedPair.map((target) => {
+        setTimeout(() => {
+          target.parentNode.classList.remove("active");
+          target.parentNode.setAttribute("data-matched", 0);
+        }, 400);
+      });
+    }
+
+    currentSelectedPair = [];
+  }
+}
+
+// ----- game logic -----
+
+// attach event handler
 table.addEventListener("click", (event) => {
-  console.log(event.target, "evnet");
+  if (event.target.parentNode.classList.contains("box-container")) {
+    console.log(event.target.parentNode.getAttribute("data-matched"), "target");
+    if (!Number(event.target.parentNode.getAttribute("data-matched"))) {
+      if (currentSelectedPair.length < 2) {
+        currentSelectedPair.push(event.target);
+      }
+    }
+    checkPair();
+  }
 });
-console.log(table);
